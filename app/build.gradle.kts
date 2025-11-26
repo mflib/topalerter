3bc5f15c8db83401d0abc48f3d37664357173cde
Mohammed Farhan <farhan.mail9632@gmail.com>
2025-11-26 11:34:06 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation(project(":topalerter"))
-//    implementation(libs.topalerter)
+//    implementation(project(":topalerter"))
+    implementation(libs.topalerter)
 }
